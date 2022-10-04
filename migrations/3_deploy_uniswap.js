db14a43bd3474a8290e16a2c0f95740cd6fe3c0f
Hai M Nguyen <vnjp22@gmail.com>
2022-10-04 15:57:05 -0700

    Migration script for NonfungibleTokenPositionDescriptor

--- migrations/3_deploy_uniswap.js
+++ migrations/3_deploy_uniswap.js
@@ -4,9 +4,13 @@ const WETH = artifacts.require("WETH");
 
 const UniswapV3FactoryJson = require('@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json');
 const SwapRouterJson = require('@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json');
+const NFTDescriptorJson = require('@uniswap/v3-periphery/artifacts/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json');
+const NonfungibleTokenPositionDescriptorJson = require('@uniswap/v3-periphery/artifacts/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json');
 
 const UniswapV3Factory = contract(UniswapV3FactoryJson);
 const SwapRouter = contract(SwapRouterJson);
+const NFTDescriptor = contract(NFTDescriptorJson);
+const NonfungibleTokenPositionDescriptor = contract(NonfungibleTokenPositionDescriptorJson);
 
 module.exports = async function(deployer, network, accounts) {
   if (network != 'test')  return;
@@ -15,7 +19,18 @@ module.exports = async function(deployer, network, accounts) {
   
   let provider = network == 'ganache' ? new web3.providers.HttpProvider("http://localhost:7545") : web3.currentProvider;
   UniswapV3Factory.setProvider(provider);
-  SwapRouter.setProvider(provider);    
+  SwapRouter.setProvider(provider);
+  NFTDescriptor.setProvider(provider);
+  NonfungibleTokenPositionDescriptor.setProvider(provider);
+  
+  await deployer.deploy(NFTDescriptor, {from: accounts[0]});
+  process.nftDescriptor = await NFTDescriptor.deployed();  
+  
+  await NonfungibleTokenPositionDescriptor.detectNetwork();  
+  await NonfungibleTokenPositionDescriptor.link(process.nftDescriptor);
+  
+  await deployer.deploy(NonfungibleTokenPositionDescriptor, wethToken.address, "ETH", {from: accounts[0]});  
+  process.nonfungibleTokenPositionDescriptor = await NonfungibleTokenPositionDescriptor.deployed();
   
   await deployer.deploy(UniswapV3Factory, {from: accounts[0]});
   process.uniswapV3Factory = await UniswapV3Factory.deployed();
