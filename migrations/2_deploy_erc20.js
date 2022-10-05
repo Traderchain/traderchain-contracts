46c91919567eb55ccca45cd7e0b337243cedcb05
Hai M Nguyen <vnjp22@gmail.com>
2022-10-05 11:15:24 -0700

    Clean up legacy truffle project files

--- migrations/2_deploy_erc20.js
+++
@@ -1,9 +0,0 @@
-const USDC = artifacts.require("USDC");
-const WETH = artifacts.require("WETH");
-
-module.exports = async function(deployer, network) {
-  if (network != 'test')  return;
-
-  await deployer.deploy(USDC);
-  await deployer.deploy(WETH);  
-};
--- migrations/4_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const {deployProxy, upgradeProxy} = require('@openzeppelin/truffle-upgrades');
-
-const OrderUpgradeable = artifacts.require("OrderUpgradeable");
-
-module.exports = async function(deployer, network) {
-  if (network != 'test')  return;
-
-  await deployProxy(OrderUpgradeable, [process.swapRouter.address], deployer);
-};
