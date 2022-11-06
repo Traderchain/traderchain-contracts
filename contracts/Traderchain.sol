3a7e6c11f3bf60c1160c4ef3fab3d56f17c97f41
Hai M Nguyen <vnjp22@gmail.com>
2022-11-05 17:00:38 -0700

    Investor can deposit USDC to a system vault

--- contracts/Traderchain.sol
+++ contracts/Traderchain.sol
@@ -4,14 +4,20 @@ pragma solidity 0.8.10;
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
+import './interfaces/IERC20.sol';
 import "./interfaces/ITradingSystem.sol";
 
 contract Traderchain is
   Context,
   AccessControlEnumerable
 {
+
+  address constant public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; 
   
   ITradingSystem public immutable tradingSystem;
+  
+  // Tracking investor funds in each system (USDC only for now)
+  mapping(uint256 => mapping(address => uint256)) public investorFunds;
   
   /***
    * Public functions
@@ -22,10 +28,26 @@ contract Traderchain is
     tradingSystem = ITradingSystem(_tradingSystem);
   }
 
+  function getInvestorFunds(uint256 systemId, address investor) public view virtual returns (uint256) {
+    return investorFunds[systemId][investor];
+  }
+
   function mintTradingSystem() public {
     address toAddress = _msgSender();
     
     tradingSystem.mint(toAddress);
+  }
+  
+  function depositFunds(uint256 systemId, address tokenAddress, uint256 amount) external payable {
+    require(tokenAddress == USDC, "Traderchain: only USDC can be deposited for now");
+    // TODO: Support funding from other ERC20 tokens
+    
+    address investor = _msgSender();            
+    address vault = tradingSystem.getSystemVault(systemId);
+    IERC20 token = IERC20(tokenAddress);
+    
+    token.transferFrom(investor, vault, amount);
+    investorFunds[systemId][investor] += amount;
   }
   
 }
--- test/Traderchain.js
+++ test/Traderchain.js
@@ -2,18 +2,25 @@ const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers"
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { 
-  ADDRESS_ZERO,  
+  ADDRESS_ZERO, USDC, WETH, USDC_WHALE,
   BigNumber, formatUnits, formatEther,
   Util
 } = require('../lib/util');
   
 describe("Traderchain", function () {  
   let signers;
+  let trader;
+  let investor1;
+  
   let tc;
   let systemContract;
   
   before(async () => {   
     signers = await ethers.getSigners();
+    trader = signers[0];
+    investor1 = signers[1];
+          
+    await Util.initContracts();      
           
     const TradingSystem = await ethers.getContractFactory("TradingSystem");
     systemContract = await TradingSystem.deploy("https://traderchain.org/system/");
@@ -25,6 +32,9 @@ describe("Traderchain", function () {
     
     const MINTER_ROLE = await systemContract.MINTER_ROLE();
     await systemContract.grantRole(MINTER_ROLE, tc.address);    
+    
+    const usdcAmount = Util.amountBN(1000, 6);
+    await Util.takeWhaleUSDC(investor1.address, usdcAmount);
   });
 
   it("Should mint a trading system", async function () {
@@ -35,5 +45,23 @@ describe("Traderchain", function () {
     await tc.connect(signers[0]).mintTradingSystem();
     expect(await systemContract.ownerOf(systemId)).to.equal(toAddress);        
   });
+  
+  it("Should deposit funds to a system vault", async function () {
+    const systemId = 1;
+    const vault = await systemContract.getSystemVault(systemId);        
+    const usdcAmount = Util.amountBN(100, 6);
+    
+    await Util.usdcToken.connect(investor1).approve(tc.address, usdcAmount);
+    await tc.connect(investor1).depositFunds(systemId, USDC, usdcAmount);
+    
+    let vaultBalance = await Util.usdcToken.balanceOf(vault);
+    Util.log({vaultBalance});
+    expect(vaultBalance).to.equal(usdcAmount);
+    
+    let investorFund = await tc.getInvestorFunds(systemId, investor1.address);
+    Util.log({investorFund});
+    expect(investorFund).to.equal(usdcAmount);
+  });
+  
   
 });
